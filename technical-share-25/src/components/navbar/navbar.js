ef76b9977c0b5cb2fc3e6b739d9e88fc75a87b91
Leo Rigotto <leorigotto@me.com>
2022-04-10 19:09:50 -0300

    inicio navbar e ajustes

---
+++ technical-share-25/src/components/navbar/navbar.js
@@ -0,0 +1,8 @@
+import { BottomNavigation } from '@mui/material'
+import React from 'react'
+
+export default function navbar() {
+  return (
+    <BottomNavigation>navbar</BottomNavigation>
+  )
+}
