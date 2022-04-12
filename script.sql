670ea33fd3c927404adce33f0fffe8b6daa67558
Leo Rigotto <leorigotto@me.com>
2022-04-11 23:14:15 -0300

    correcoes de script, implementacao do autocomplete

--- script.sql
+++ script.sql
@@ -8,40 +8,24 @@ CREATE TABLE Users(
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   photo VARCHAR(255) NOT NULL,
-  bio VARCHAR(255) NOT NULL,
-  links VARCHAR(255) NOT NULL,
-  role VARCHAR(255) NOT NULL,
-  tags VARCHAR(255),
-  mentorID INT,
-  FOREIGN KEY (mentorID) REFERENCES Users(id)
-);
-
-CREATE TABLE Messages(
-	message_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    userID INT NOT NULL,
-    mentorID INT NOT NULL,
-    body TEXT NOT NULL,
-    sent_date DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (userID) REFERENCES Users(id),
-    FOREIGN KEY (mentorID) REFERENCES Users(id)
+  phone VARCHAR(255) NOT NULL,
+  role VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Posts(
-  post_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  post_id VARCHAR(255) PRIMARY KEY NOT NULL,
   userID INT,
   title VARCHAR(255) NOT NULL,
   body TEXT NOT NULL,
   votes INT DEFAULT 0 NOT NULL,
   post_date DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  post_tags VARCHAR(255),
-  question BOOLEAN,
   FOREIGN KEY (userID) REFERENCES Users(id)
 );
 
 CREATE TABLE Comments(
 	comment_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     comment_userID INT,
-    postID INT,
+    postID VARCHAR(255),
 	comment_body TEXT NOT NULL,
     comment_votes INT DEFAULT 0,
 	comment_date DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -49,14 +33,12 @@ CREATE TABLE Comments(
     FOREIGN KEY (postID) REFERENCES Posts(post_id)
 );
 
+CREATE TABLE Skills(
+	skill_name VARCHAR(255) NOT NULL,
+    userID INT,
+    postID VARCHAR(255),
+    FOREIGN KEY (userID) REFERENCES Users(id),
+    FOREIGN KEY (postID) REFERENCES Posts(post_id)
+);
 
-DROP TABLE Comments;
-
-SELECT id, user_name, photo, post_id, title, body, post_date, votes, comments FROM Users JOIN Posts ON Posts.userID = Users.id;
-
-SELECT id, user_name, photo, post_id, title, body, post_date, votes, comment_id, comment_userID, comment_body, comment_votes, comment_date
-      FROM Users      
-      JOIN Posts 
-      ON Posts.userID = Users.id
-      LEFT JOIN Comments
-      ON Comments.postID = Posts.post_id;
+DROP TABLE Skills, Comments, Posts;
