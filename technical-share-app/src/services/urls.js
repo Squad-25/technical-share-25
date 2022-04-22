796e23dfaa8e352baf653caea4633b729a6ec54c
Leo Rigotto <leorigotto@me.com>
2022-04-21 23:26:14 -0300

    ajustes de deploy

--- technical-share-app/src/services/urls.js
+++ technical-share-app/src/services/urls.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:3003"
+export const BASE_URL = "https://technical-share-squad25.herokuapp.com"
 
 export const userID = (localStorage.getItem('user_id') ? localStorage.getItem('user_id') : '4')
